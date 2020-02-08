0ef41f950e42338c065f262159c8585c359514c5
Tan Keller <ersoichso@gmail.com>
2020-02-08 23:24:50 +0100

    Remove bloat

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,15 +1,8 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import Header from "../header/header.js"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -26,13 +19,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
