24fa139d2ebac6cae7c140283ef988e8a2d6e88b
Tan Keller <ersoichso@gmail.com>
2020-02-13 19:55:16 +0100

    Add component for logo

---
+++ src/components/logo/logo.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import "typeface-rock-salt"
+import "./logo.css"
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <Link to="/">Tan Keller</Link>
+    </div>
+  )
+}
+
+export default Logo
