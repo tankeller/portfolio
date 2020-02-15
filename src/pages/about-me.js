ab589ad099e8c8ae2761d4f5ea62b8c2ed6528c7
Tan Keller <ersoichso@gmail.com>
2020-02-15 18:40:22 +0100

    Add about me Page

---
+++ src/pages/about-me.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout/layout"
+
+const aboutMe = () => {
+  return (
+    <Layout>
+      <h1>Hello my Name is! My name is!</h1>
+    </Layout>
+  )
+}
+
+export default aboutMe
