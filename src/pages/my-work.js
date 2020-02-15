3dc163b374f3a6ee281533bff8aa377b6b21d6ba
Tan Keller <ersoichso@gmail.com>
2020-02-15 18:40:34 +0100

    Add reference page

---
+++ src/pages/my-work.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout/layout"
+
+const myWork = () => {
+  return (
+    <Layout>
+      <h1>This is my work</h1>
+    </Layout>
+  )
+}
+
+export default myWork
